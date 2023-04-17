e7805c8f871008058020190547e7859ad954bf65
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2023-04-17 10:47:51 -0300

    Component sidebar (#7)
    * Desenvolvendo o header da aplicação
    
    * Desenvolvendo o componente SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { SideBar } from "./components/SideBar"
+
+import styles from "./App.module.css"
+import "./global.css"
 
 import "./global.css"
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <main>posts</main>
+      </div>
+    </>
   )
 }
 
