28dd62126dd62fa230c581a7413bbd47b42b9fa2
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2023-04-19 16:21:11 -0300

    desenvolvendo o componente de post (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/Header"
 import { SideBar } from "./components/SideBar"
+import { Post } from "./components/Post"
 
 import styles from "./App.module.css"
 import "./global.css"
 
-import "./global.css"
 
 function App() {
   return (
@@ -12,11 +12,14 @@ function App() {
       <Header/>
       <div className={styles.wrapper}>
         <SideBar/>
-        <main>posts</main>
+        <main>
+          <Post author="Lucas Ribeiro" content="Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"/>
+          <Post author="Lucas Ribeiro" content="Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"/>
+          <Post author="Lucas Ribeiro" content="Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"/>
+        </main>
       </div>
     </>
   )
 }
 
 export default App
-
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,38 @@
+import styles from "./Post.module.css"
+
 export function Post(props){
   return(
-    <div>
-      <h1>{props.author}</h1>
-      <p>{props.content}</p>
+    <div className={styles.postContent}>
+      <header className={styles.postHeader}>
+        <div className={styles.profile}>
+          <img 
+            src="https://github.com/LUKASRIB15.png"
+            width={50}
+            height={50}
+          />
+          <div>
+            <strong>{props.author}</strong>
+            <span>Dev Full-stack</span>
+          </div>
+        </div>
+        <time>Publicado há 1h</time>
+      </header>
+      <main className={styles.postMain}>
+        <p>
+        Fala galeraa 👋
+        <br/><br/>
+        {props.content}
+        <br/><br/>
+        <a href="#">👉 jane.design/doctorcare</a>
+        <br/><br/>
+        <a href="#">#novoprojeto</a>{' '}<a href="#">#nlw</a>{' '}<a href="#">#rocketseat</a>
+        </p>
+      </main>
+      <form className={styles.postForm}>
+        <label for="textareaContent">Deixe seu feedback</label>
+        <textarea id="textareaContent" placeholder="Escreva um comentário..."/>
+        <button type="submit">Publicar</button>
+      </form>
     </div>
   )
 }
